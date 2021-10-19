d83ed3494c5ad0377e00085b3a2f98eacca2db8f
digital-souris <85878712+digital-souris@users.noreply.github.com>
2021-10-19 16:46:37 +0300

    Initial commit

---
+++ project.config.js
@@ -0,0 +1,14 @@
+// Override Defaults
+
+module.exports = {
+    srcPath: "./src",
+    templatePath: "./src/pages",
+    buildPath: "./dist",
+    minifyHTML: true,
+    minifyJS: true,
+    minifyCSS: true,
+    homePage: "index.html",
+    templateFiles: [
+        "./src/pages/index.jade",
+    ]
+};
